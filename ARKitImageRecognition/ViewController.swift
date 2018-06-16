033eb1b02a46505799dcb458238037971a261aeb
Gurinder Singh <gsingh622@yahoo.com>
2018-06-16 19:54:47 -0400

    init

---
+++ ARKitImageRecognition/ViewController.swift
@@ -0,0 +1,162 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Main view controller for the AR experience.
+*/
+
+import ARKit
+import SceneKit
+import UIKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet var sceneView: ARSCNView!
+    
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    
+    /// The view controller that displays the status and "restart experience" UI.
+    lazy var statusViewController: StatusViewController = {
+        return childViewControllers.lazy.flatMap({ $0 as? StatusViewController }).first!
+    }()
+    
+    /// A serial queue for thread safety when modifying the SceneKit node graph.
+    let updateQueue = DispatchQueue(label: Bundle.main.bundleIdentifier! +
+        ".serialSceneKitQueue")
+    
+    /// Convenience accessor for the session owned by ARSCNView.
+    var session: ARSession {
+        return sceneView.session
+    }
+    
+    // MARK: - View Controller Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        sceneView.session.delegate = self
+
+        // Hook up status view controller callback(s).
+        statusViewController.restartExperienceHandler = { [unowned self] in
+            self.restartExperience()
+        }
+    }
+
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		
+		// Prevent the screen from being dimmed to avoid interuppting the AR experience.
+		UIApplication.shared.isIdleTimerDisabled = true
+
+        // Start the AR experience
+        resetTracking()
+	}
+	
+	override func viewWillDisappear(_ animated: Bool) {
+		super.viewWillDisappear(animated)
+
+        session.pause()
+	}
+
+    // MARK: - Session management (Image detection setup)
+    
+    /// Prevents restarting the session while a restart is in progress.
+    var isRestartAvailable = true
+
+    /// Creates a new AR configuration to run on the `session`.
+    /// - Tag: ARReferenceImage-Loading
+	func resetTracking() {
+        
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
+            fatalError("Missing expected asset catalog resources.")
+        }
+        
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.detectionImages = referenceImages
+        session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+
+        statusViewController.scheduleMessage("Look around to detect images", inSeconds: 7.5, messageType: .contentPlacement)
+	}
+
+    // MARK: - ARSCNViewDelegate (Image detection results)
+    /// - Tag: ARImageAnchor-Visualizing
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let imageAnchor = anchor as? ARImageAnchor else { return }
+        let referenceImage = imageAnchor.referenceImage
+        updateQueue.async {
+            
+            // Create a plane to visualize the initial position of the detected image.
+            let plane = SCNPlane(width: referenceImage.physicalSize.width,
+                                 height: referenceImage.physicalSize.height)
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.opacity = 0.25
+            
+            /*
+             `SCNPlane` is vertically oriented in its local coordinate space, but
+             `ARImageAnchor` assumes the image is horizontal in its local space, so
+             rotate the plane to match.
+             */
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            /*
+             Image anchors are not tracked after initial detection, so create an
+             animation that limits the duration for which the plane visualization appears.
+             */
+            planeNode.runAction(self.imageHighlightAction)
+            
+            // Add the plane visualization to the scene.
+            node.addChildNode(planeNode)
+            
+            //add item to plane
+            //animate item
+            // Create a new scene
+            let scene = SCNScene(named: "art.scnassets/ship.scn")!
+            //scene.rootNode.position = planeNode.position
+            // Set the scene to the view
+            self.sceneView.scene.rootNode.addChildNode(scene.rootNode)
+            
+            //let grow = SCNAction.scale(to: 1 * 2 + 1, duration: 1)
+            //scene.rootNode.runAction(grow)
+            
+            //scene.rootNode.scale = SCNVector3(-3,1,1)
+            let tempScale: CGFloat = 1.0
+            scene.rootNode.scale = SCNVector3(tempScale, tempScale, tempScale)
+            // grow from 1 to tempScale, then back to 1
+            let grow = SCNAction.scale(to: tempScale, duration: 4.1)
+            let shrink = SCNAction.scale(to: 0.1, duration: 2.1)
+            let sequence = SCNAction.sequence([shrink, grow])
+            //scene.rootNode.runAction(SCNAction.repeatForever(sequence))
+            scene.rootNode.runAction(sequence)
+
+            //let action = SCNAction.repeatForever(SCNAction.rotate(by: .pi, around: SCNVector3(0, 1 , 0), duration: 0.5))
+            //scene.rootNode.runAction(SCNAction.repeatForever(action))
+            
+            //scene.rootNode.runAction(SCNAction.repeatForever(grow))
+            
+            
+
+
+            //planeNode.addChildNode(scene.rootNode)
+        }
+
+        DispatchQueue.main.async {
+            let imageName = referenceImage.name ?? ""
+            self.statusViewController.cancelAllScheduledMessages()
+            self.statusViewController.showMessage("Detected image “\(imageName)”")
+    
+            
+        }
+    }
+
+    var imageHighlightAction: SCNAction {
+        return .sequence([
+            .wait(duration: 0.25),
+            .fadeOpacity(to: 0.85, duration: 0.25),
+            .fadeOpacity(to: 0.15, duration: 0.25),
+            .fadeOpacity(to: 0.85, duration: 0.25),
+            .fadeOut(duration: 0.5),
+            .removeFromParentNode()
+        ])
+    }
+}
